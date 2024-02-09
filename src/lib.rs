8d45642e2e3f3c12c36f296d7f36426d93f73370
SNS <stephen.nelsonsmith@ticketmaster.co.uk>
2024-02-09 06:07:24 +0000

    Change: Make callsign parser way more permissive
    While it might be instructive to try to build a parser which matches
    all varieties of acceptable callsigns, its going to be tremendously
    involved, so instead, we'll go the other way, and start with something
    that will match most things, and then add to it.  For our purposes it's
    generally better to match more than less, and we can refine for now.
    
    The currently failing test drives a complication - we need to handle
    suffixes like /p /mm /qrp etc.

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
-use nom::character::complete::{char, satisfy};
-use nom::multi::many_m_n;
-use nom::{combinator::recognize, sequence::tuple, IResult};
+use nom::character::complete::alphanumeric1;
+use nom::IResult;
 use std::str::FromStr;
+use nom::combinator::verify;
 
 #[derive(Debug, PartialEq, Eq)]
 struct Callsign(String);
@@ -9,12 +9,13 @@ struct Callsign(String);
 #[derive(Debug, PartialEq, Eq)]
 struct ParseCallsignError;
 
-fn callsign_parser(notes: &str) -> IResult<&str, &str> {
-    recognize(tuple((
-        char('g'),
-        satisfy(|c: char| c.is_digit(10) && c != '9'),
-        many_m_n(2, 3, satisfy(|c: char| c.is_alphabetic())),
-    )))(notes)
+fn contains_both_letter_and_digit(input: &str) -> bool {
+    let has_digit = input.chars().any(|c| c.is_digit(10));
+    let has_alpha = input.chars().any(|c| c.is_alphabetic());
+    has_digit && has_alpha
+}
+fn callsign_parser(input: &str) -> IResult<&str, &str> {
+    verify(alphanumeric1, contains_both_letter_and_digit)(input)
 }
 
 impl FromStr for Callsign {
@@ -33,15 +34,42 @@ mod tests {
     use super::*;
 
     #[test]
-    fn parses_english_callsign_correctly() {
+    fn parses_english_full_callsign_correctly() {
         let notes = "g4emm";
         let result = notes.parse::<Callsign>();
         assert_eq!(result, Ok(Callsign("g4emm".to_string())));
     }
 
     #[test]
-    fn passes_over_anything_that_isnt_callsign() {
+    fn parses_english_intermediate_callsign_correctly() {
+        let notes = "2e0ihm";
+        let result = notes.parse::<Callsign>();
+        assert_eq!(result, Ok(Callsign("2e0ihm".to_string())));
+    }
+
+    #[test]
+    fn parses_english_foundation_callsign_correctly() {
+        let notes = "m7dqd";
+        let result = notes.parse::<Callsign>();
+        assert_eq!(result, Ok(Callsign("m7dqd".to_string())));
+    }
+
+    #[test]
+    fn parses_portable_and_other_suffixes() {
+        let notes = "g0qwe/p";
+        let result = notes.parse::<Callsign>();
+        assert_eq!(result, Ok(Callsign("g0qwe/p".to_string())));
+    }
+
+    #[test]
+    fn no_numbers_is_invalid() {
         let result = "notacall".parse::<Callsign>();
+        assert!(result.is_err());
+    }
+
+    #[test]
+    fn no_letters_is_invalid() {
+        let result = "12345".parse::<Callsign>();
         assert!(result.is_err());
     }
 }
